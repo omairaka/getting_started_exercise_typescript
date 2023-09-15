434244ec95ebbeff559e41b98e7b44910e127678
Muhammad Omair <sunomair@gmail.com>
2023-09-15 11:12:55 +0500

    Optimized

--- getting_started_exercise.md/31/No_user.js
+++ getting_started_exercise.md/31/No_user.js
@@ -1,14 +1,10 @@
 "use strict";
 var users = ["Omair ", "Shahzain", "Rahul", "Admin", "Afsar"];
-for (var i = 0; i < users.length; i++) {
-    if (users[i] == "Admin") {
-        console.log(`Hello ${users[i]} would you like to see a status report?\n`);
-    }
-    else
-        console.log(`Hello ${users[i]} thank you for logging in again \n`);
+for (var i = 0; i < 5; i++) {
+    users.splice(i);
 }
-console.log("Array Emptied");
-var users = [];
-if (users.length <= 0) {
-    console.log("We need to find some users!");
+if (users.length == 0) {
+    console.log("list is empty we need to find some users");
 }
+else
+    console.log("list is not empty");
